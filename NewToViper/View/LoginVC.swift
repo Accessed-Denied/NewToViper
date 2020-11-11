7213c70a322dff17693fcd59f587780cdfafa1ed
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-11-12 00:17:11 +0530

    modified code of loginVC

---
+++ NewToViper/View/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  NewToViper
+//
+//  Created by MACBOOK on 11/11/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+import SainiUtils
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var emailView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configUI() {
+//        emailView.
+    }
+}

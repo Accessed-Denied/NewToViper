bb373fca124fb722c15efd977a55e6a0eb1d2821
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-11-15 19:53:57 +0530

    outlets added and modified code of loginVC

--- NewToViper/View/LoginVC.swift
+++ NewToViper/View/LoginVC.swift
@@ -11,6 +11,11 @@ import SainiUtils
 
 class LoginVC: UIViewController {
     
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var emailView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,8 +23,28 @@ class LoginVC: UIViewController {
         configUI()
     }
     
+    //MARK: - configUI
     private func configUI() {
+        emailView.layer.borderWidth = 1
+        emailView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        emailView.sainiCornerRadius(radius: 25)
         
+        passwordView.layer.borderWidth = 1
+        passwordView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        passwordView.sainiCornerRadius(radius: 25)
+        
+        loginBtn.sainiCornerRadius(radius: 25)
     }
     
+    //MARK: - loginBtnIsPressed
+    @IBAction func loginBtnIsPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - forgotPasswordBtnIsPressed
+    @IBAction func forgotPasswordBtnIsPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - registerBtnIsPressed
+    @IBAction func registerBtnIsPressed(_ sender: UIButton) {
+    }
 }

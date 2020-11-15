e8618bdf59520ed98cc0d2c1d0d0b0a86b32a4dd
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-11-15 20:22:08 +0530

    outlets added and modified code of SignUpVC

--- NewToViper/View/SignUpVC.swift
+++ NewToViper/View/SignUpVC.swift
@@ -10,21 +10,49 @@ import UIKit
 
 class SignUpVC: UIViewController {
 
+    @IBOutlet weak var registerBtn: UIButton!
+    @IBOutlet weak var confirmPasswordTextfield: UITextField!
+    @IBOutlet weak var confirmPasswordView: UIView!
+    @IBOutlet weak var passwordTextfield: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var emailView: UIView!
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var nameView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        configUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - configUI
+    private func configUI() {
+        nameView.layer.borderWidth = 1
+        nameView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        nameView.sainiCornerRadius(radius: 25)
+        
+        emailView.layer.borderWidth = 1
+        emailView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        emailView.sainiCornerRadius(radius: 25)
+        
+        passwordView.layer.borderWidth = 1
+        passwordView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        passwordView.sainiCornerRadius(radius: 25)
+        
+        confirmPasswordView.layer.borderWidth = 1
+        confirmPasswordView.layer.borderColor = #colorLiteral(red: 0.8980392157, green: 0.4196078431, blue: 0.5019607843, alpha: 1)
+        confirmPasswordView.sainiCornerRadius(radius: 25)
+        
+        registerBtn.sainiCornerRadius(radius: 25)
     }
-    */
-
+    
+    //MARK: - registerBtnIsPressed
+    @IBAction func registerBtnIsPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - alreadyHaveAccountBtnIsPressed
+    @IBAction func alreadyHaveAccountBtnIsPressed(_ sender: UIButton) {
+    }
+    
 }
